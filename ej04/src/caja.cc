8d63ccee8b9fab2663f8fd5de15906339ec613c5
As4ncab <alu0101482323@ull.edu.es>
2024-01-01 19:31:46 +0000

    .cc added

--- ej04/src/caja.cc
+++ ej04/src/caja.cc
@@ -1,59 +1,62 @@
 /**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado de Ingeniería Informática
+ * Informática Básica 2020-2021
+ *
  * @file caja.cc
- * @author Alexia Sánchez Cabrera (alu0101482323@ull.edu.es)
+ * @author S4yuM4ng0    (s4yum4ng0@random.com)
+ * @date Jun 24 2021
  *
- * @brief Complete la clase con los constructores que considere oportunos y la
- *        sobrecarga de operadores ==, extracción a través ostream e inserción a
- *        través de istream. (2 pts)
+ * @brief Library with class and function definitions
  *
- * @version 0.1
- * @date 2023-06-09
- *
- * @copyright Copyright (c) 2023
- *
+ * @bug There are no known bugs
  */
 
 #include "caja.h"
 
-// Constructores
+///< Constructores
+
 /**
- * @brief Construye un nuevo objeto Box
+ * @brief Construct a new Box::Box object
  *
  * @param l
  * @param b
  * @param h
  */
-Box::Box(const double l, const double b, const double h) {
+Box::Box(const double& l, const double& b, const double& h) {
   length_ = l;
   breadth_ = b;
   height_ = h;
 }
 
-// Setters
+///< Setters
+
 /**
- * @brief Da el valor l a lenght_
+ * @brief Gives value l to length_
  *
  * @param l
  */
 void Box::SetLength(const double& l) { length_ = l; }
 
 /**
- * @brief Da el valor b a breadth_
+ * @brief Gives value b to breadth_
  *
  * @param b
  */
 void Box::SetBreadth(const double& b) { breadth_ = b; }
 
 /**
- * @brief Da el valor h a height_
+ * @brief Gives value h to height_
  *
  * @param h
  */
 void Box::SetHeight(const double& h) { height_ = h; }
 
-// Sobrecarga de operadores
+///< Operator overloadings
+
 /**
- * @brief Compara dos objetos Box y devuelve 1 si son iguales, 0 si no lo son
+ * @brief Operator "==" overloading for comparing Box objects
  *
  * @param caja
  * @return true
@@ -62,18 +65,16 @@ void Box::SetHeight(const double& h) { height_ = h; }
 bool Box::operator==(const Box& caja) {
   if ((length_ == caja.length_) && (breadth_ == caja.breadth_) &&
       (height_ == caja.height_)) {
-    return true;
+    return 1;
   }
-
-  return false;
+  return 0;
 }
 
 /**
- * @brief Sobrecarga del operador de inserción (imprime por pantalla el objeto
- *        Box)
+ * @brief Operator "<<" overloading for printing Box objects
  *
  * @param os
- * @param Box
+ * @param box
  * @return std::ostream&
  */
 std::ostream& operator<<(std::ostream& os, Box& box) {
@@ -85,42 +86,15 @@ std::ostream& operator<<(std::ostream& os, Box& box) {
 }
 
 /**
- * @brief Sobrecarga del operador de extracción (obtiene los datos para los
- *        atributos length_, breadth_ y height_ respectivamente)
+ * @brief Operator ">>" overloading for initialising Box object with the data
+ *        typed in by the user
  *
  * @param is
- * @param Box
+ * @param box
  * @return std::istream&
  */
 std::istream& operator>>(std::istream& is, Box& box) {
   is >> box.length_ >> box.breadth_ >> box.height_;
 
   return is;
-}
-
-// Funciones
-/**
- * @brief Función que imprime el propósito del programa
- *
- */
-void PrintProgramPurpose() {
-  std::cout << "This program changes the values of the dimensions of 3 boxes "
-               "and prints them for the user to see.\n"
-            << std::endl;
-}
-
-/**
- * @brief Indica si los dos objetos caja son iguales o no e imprime un mensaje
- *        por pantalla para notificar al usuario
- *
- * @param box_1
- * @param box_2
- */
-void AreEqual(Box& box_1, Box& box_2) {
-  if (box_1 == box_2) {
-    std::cout << "The boxes are equal" << std::endl;
-
-  } else {
-    std::cout << "The boxes are different" << std::endl;
-  }
 }
